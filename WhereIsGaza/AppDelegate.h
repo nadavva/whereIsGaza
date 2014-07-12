a765f6471e4fcf4dc315009a06588883cb22857d
Nadav Vanunu <nadav.vanunu@hp.com>
2014-07-12 18:09:51 +0300

    Initial Commit

---
+++ WhereIsGaza/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WhereIsGaza
+//
+//  Created by Nadav Vanunu on 7/12/14.
+//  Copyright (c) 2014 Nadav Vanunu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhereIsGaza/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WhereIsGaza
+//
+//  Created by Nadav Vanunu on 7/12/14.
+//  Copyright (c) 2014 Nadav Vanunu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
