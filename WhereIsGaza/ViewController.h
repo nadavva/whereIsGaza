1fc423d5723cf0b4c47afbfb00d98b82dc7dd42e
Nadav Vanunu <nadavva@gmail.com>
2014-07-15 22:20:45 +0300

    gyro events and new icon

--- WhereIsGaza/ViewController.h
+++ WhereIsGaza/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate> {
     CLLocationManager *locationManager;
@@ -15,5 +16,9 @@
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (nonatomic, retain) MKPolyline *routeLine; //your line
 @property (nonatomic, retain) MKPolylineView *routeLineView; //overlay view
+@property (nonatomic) MKUserLocation * lastLocation;
+@property (nonatomic) CMMotionManager *motionManager;
+
+- (IBAction)tapOnMapOccured:(id)sender;
 
 @end
--- WhereIsGaza/ViewController.m
+++ WhereIsGaza/ViewController.m
@@ -20,8 +20,8 @@
     self.mapView.delegate = self;
 
 	// Do any additional setup after loading the view, typically from a nib.
-    float spanX = 0.00725;
-    float spanY = 0.00725;
+    float spanX = 0.00025;
+    float spanY = 0.00025;
     MKCoordinateRegion region;
     region.center.latitude = self.mapView.userLocation.coordinate.latitude;
     region.center.longitude = self.mapView.userLocation.coordinate.longitude;
@@ -36,32 +36,76 @@
     locationManager.distanceFilter = kCLDistanceFilterNone;
     locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     [locationManager startUpdatingLocation];
+    
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+    
+    
+    //Gyroscope
+    if([self.motionManager isGyroAvailable])
+    {
+        /* Start the gyroscope if it is not active already */
+        if([self.motionManager isGyroActive] == NO)
+        {
+            /* Update us 2 times a second */
+            [self.motionManager setGyroUpdateInterval:1.0f / 2.0f];
+            
+            /* Add on a handler block object */
+            
+            /* Receive the gyroscope data on this block */
+            [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue]
+                                            withHandler:^(CMGyroData *gyroData, NSError *error)
+             {
+//                 NSString *x = [[NSString alloc] initWithFormat:@"%.02f",gyroData.rotationRate.x];
+//                 NSLog(@"X = %@",x);
+//                 
+//                 NSString *y = [[NSString alloc] initWithFormat:@"%.02f",gyroData.rotationRate.y];
+//                 NSLog(@"Y = %@",y);
+//                 
+//                 NSString *z = [[NSString alloc] initWithFormat:@"%.02f",gyroData.rotationRate.z];
+//                 NSLog(@"Z = %@",z);
+//                 
+//                 NSLog(@"-------------------------------------------------------");
+                 
+                 if (gyroData.rotationRate.z > 0.2) {
+                     [self updateMap];
+                 }
+
+             }];
+        }
+    }
+    else
+    {
+        NSLog(@"Gyroscope not Available!");
+    }
 }
 
 
-- (void)updateMap:(MKUserLocation *)location
+- (void) updateMap
 {
+    [self focusOnRegion];
 
-    float spanX = 0.00725;
-    float spanY = 0.00725;
-    MKCoordinateRegion region;
-    region.center.latitude = location.coordinate.latitude;
-    region.center.longitude = location.coordinate.longitude;
-    region.span.latitudeDelta = spanX;
-    region.span.longitudeDelta = spanY;
-    
-    [self.mapView setRegion:region animated:YES];
-    
     CLLocationCoordinate2D coordinateArray[2];
-    coordinateArray[0] = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude);
+    coordinateArray[0] = CLLocationCoordinate2DMake(_lastLocation.coordinate.latitude, _lastLocation.coordinate.longitude);
     coordinateArray[1] = CLLocationCoordinate2DMake(31.4167, 34.3333);
 
-    if (self.routeLine) {
-        [self.mapView removeOverlay:self.routeLine];
-    }
     self.routeLine = [MKPolyline polylineWithCoordinates:coordinateArray count:2];
     //[self.mapView setVisibleMapRect:[self.routeLine boundingMapRect]]; //If you want the route to be visible
     [self.mapView addOverlay:self.routeLine];
+}
+
+- (void) focusOnRegion
+{
+    [self.mapView setCenterCoordinate:_lastLocation.coordinate animated:YES];
+    float spanX = 0.00025;
+    float spanY = 0.00025;
+    MKCoordinateRegion region;
+    region.center.latitude = _lastLocation.coordinate.latitude;
+    region.center.longitude = _lastLocation.coordinate.longitude;
+    region.span.latitudeDelta = spanX;
+    region.span.longitudeDelta = spanY;
+    [self.mapView setRegion:region animated:YES];
+
 }
 
 -(MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id<MKOverlay>)overlay
@@ -74,20 +118,19 @@
             self.routeLineView.fillColor = [UIColor redColor];
             self.routeLineView.strokeColor = [UIColor redColor];
             self.routeLineView.lineWidth = 5;
-            
         }
-        
         return self.routeLineView;
     }
-    
     return nil;
 }
 
 -(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
-    [self.mapView setCenterCoordinate:userLocation.coordinate animated:YES];
-    //[self.mapView addOverlay:self.routeLine];
-    [self updateMap:userLocation];
+    _lastLocation = userLocation;
+    [self updateMap];
 }
 
 
+- (IBAction)tapOnMapOccured:(id)sender {
+    [self focusOnRegion];
+}
 @end
